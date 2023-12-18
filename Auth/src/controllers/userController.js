a83845c335a5424b3f5d19b2b617c6770f5f3511
CyanMango377 <andr.sousasilva@outlook.com>
2023-12-18 00:04:58 +0000

    Final

--- Auth/src/controllers/userController.js
+++ Auth/src/controllers/userController.js
@@ -1,7 +1,7 @@
 // Imports
 require("dotenv").config();
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // Models
